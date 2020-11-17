9ac3c00de7e42e80e7632cd943ae4ac7f402a20a
Quentin Small <qsmall@wayfair.com>
2020-11-16 19:48:46 -0500

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-b349ea32b9d15bb340dd.js"
+    "url": "webpack-runtime-e8ad77f51197d37fc13f.js"
   },
   {
     "url": "framework-dab657cf2d102cb8d2b9.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "8f3134d97ae897ebcdf1bac993598d21"
+    "revision": "c203a6a0a84705c826be0557a6b0f44f"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b81c43cf3b4ef9854106.js"
